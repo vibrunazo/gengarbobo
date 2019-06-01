b148d2a1f6d45b4df88ab2b6e78e79cb2c2659c9
Vandré Brunazo <vibrunazo@gmail.com>
2019-06-01 18:15:16 -0300

    add material nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainnavComponent } from './mainnav/mainnav.component';
+import { MatSidenavModule, MatToolbarModule, MatNavList, MatIconModule } from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainnavComponent,
+    MatNavList
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
