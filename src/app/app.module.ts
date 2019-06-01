a7f25a570dde81f722bc4d794e145db9e1721cbc
Vandré Brunazo <vibrunazo@gmail.com>
2019-06-01 18:21:14 -0300

    fix button imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainnavComponent } from './mainnav/mainnav.component';
-import { MatSidenavModule, MatToolbarModule, MatNavList, MatIconModule } from '@angular/material';
+import { MatSidenavModule, MatToolbarModule, MatNavList, MatIconModule, MatButtonModule, MatListModule } from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainnavComponent,
-    MatNavList
   ],
   imports: [
     BrowserModule,
@@ -19,7 +18,9 @@ import { MatSidenavModule, MatToolbarModule, MatNavList, MatIconModule } from '@
     BrowserAnimationsModule,
     MatSidenavModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatButtonModule,
+    MatListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
