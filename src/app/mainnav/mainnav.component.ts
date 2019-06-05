c1e094169df9974961b689ceee92953d073fd4f2
Vandré Brunazo <vibrunazo@gmail.com>
2019-06-04 22:25:21 -0300

    closes sidebar on click

--- src/app/mainnav/mainnav.component.ts
+++ src/app/mainnav/mainnav.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { MatSidenav, MatButton } from '@angular/material';
 
 @Component({
   selector: 'app-mainnav',
@@ -9,12 +10,20 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./mainnav.component.scss']
 })
 export class MainnavComponent {
+  @ViewChild(MatSidenav, { static: false }) sidenav: MatSidenav;
+  @ViewChild('handsetmenu', { static: false }) handsetmenu: MatButton;
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches)
-    );
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(map(result => result.matches));
 
   constructor(private breakpointObserver: BreakpointObserver) {}
 
+  closeSidenav() {
+    // only closes sidenav if in handset mode
+    // we're sure we're in handset mode if the handset menu button was found
+    if (this.handsetmenu) {
+      this.sidenav.close();
+    }
+  }
 }
