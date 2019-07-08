5fe05bcd20e902cfaa3e835cfa10081ac3ebe529
Vandré Israel Brunazo <vibrunazo@gmail.com>
2019-07-07 23:43:07 -0300

    add move names to dex

--- src/app/dex/pktable/pktable.component.ts
+++ src/app/dex/pktable/pktable.component.ts
@@ -1,6 +1,7 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator, MatSort, MatTable } from '@angular/material';
 import { PktableDataSource, PktableItem } from './pktable-datasource';
+import { Move } from 'src/app/shared/shared.module';
 
 @Component({
   selector: 'app-pktable',
@@ -24,5 +25,17 @@ export class PktableComponent implements AfterViewInit, OnInit {
     this.dataSource.sort = this.sort;
     this.dataSource.paginator = this.paginator;
     this.table.dataSource = this.dataSource;
+  }
+
+  // returns the HTML to write in the cell for a fast or charged move. Given the array of move ids from that cell
+  getMoveHTML(moveIds: string[]): string {
+    let html = '';
+    const moves: Move[] = [];
+    moveIds.forEach((m, i) => {
+      const thisMove = Move.findMoveById(m);
+      moves.push(thisMove);
+      html += thisMove.name + (i + 1 < moveIds.length ? ', ' : '');
+    });
+    return html;
   }
 }
