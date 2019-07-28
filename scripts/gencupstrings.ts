07abf6ce061edf25d1551a1b4f8b6d1cf784d853
Vandré Israel Brunazo <vibrunazo@gmail.com>
2019-07-28 20:44:15 -0300

    scripts to export data to pvpoke

---
+++ scripts/gencupstrings.ts
@@ -0,0 +1,87 @@
+// to run this script, use ts-node from the root directory:
+// npx ts-node scripts/gencupstrings.ts
+
+// this script loads the data for all pokémon from the gamemaster
+// and then writes a list of pokémon names that are eligible for PvP Cups
+// these lists are used by PvPoke.com to generate rankings between only eligible pokémons
+// the lists are saved to scripts/out/cups.json
+
+import * as fs from 'fs';
+import Pokemon from '../src/app/shared/shared.module';
+import LEGENDARIES from '../src/app/shared/legendaries';
+import LIGUINHA3 from '../src/app/shared/liguinha3';
+import { log } from 'util';
+
+console.log('update script running');
+const cups = {
+  kanto: {
+    allowedList: []
+  },
+  johto: {
+    allowedList: []
+  },
+  hoenn: {
+    allowedList: [],
+    bannedList: []
+  },
+  sinnoh: {
+    allowedList: []
+  },
+  uc: {
+    allowedList: [],
+    bannedList: LEGENDARIES
+  },
+  liguinha3: {
+    allowedList: []
+  }
+};
+
+genTableItems();
+saveToFile();
+
+function genTableItems() {
+  const dex = Pokemon.dex;
+  let liguinha3 = 0;
+  dex.forEach(p => {
+    if (p.dex <= 151 && !p.speciesId.includes('alola')) {
+      cups.kanto.allowedList.push(p.speciesId);
+    }
+    if (p.dex >= 152 && p.dex <= 251) {
+      cups.johto.allowedList.push(p.speciesId);
+    }
+    if (p.dex >= 252 && p.dex <= 386 && p.speciesId !== 'jirachi') {
+      cups.hoenn.allowedList.push(p.speciesId);
+    }
+    if (p.dex >= 387 && p.dex <= 493) {
+      cups.sinnoh.allowedList.push(p.speciesId);
+    }
+    // if (!LEGENDARIES.includes(p.speciesId)) {
+    //   cups.uc.allowedList.push(p.speciesId);
+    // }
+    if (LIGUINHA3.includes(p.dex) && p.speciesId !== 'persian') {
+      cups.liguinha3.allowedList.push(p.speciesId);
+      liguinha3++;
+    }
+
+
+  });
+  console.log('liguinha3: ' + liguinha3);
+
+  function filterTypes(types) {
+    return types;
+  }
+}
+
+function saveToFile() {
+  const path = './scripts/out/cups.json';
+  const json = JSON.stringify(cups);
+  // const filetext = 'export const ROWS = ' + json + ';';
+  fs.writeFile(path, json, 'utf8', e => {
+    console.log('creating json: ' + path);
+    if (e) {
+      console.log('' + e);
+    } else {
+      console.log('sucess');
+    }
+  });
+}
--- scripts/update.ts
+++ scripts/update.ts
@@ -1,3 +1,12 @@
+// to run this script, use ts-node from the root directory:
+// npx ts-node scripts/update.ts
+
+// this script loads the data for all pokémon from the gamemaster
+// and writes a table easy to be used by the dex component at src/app/dex/dex.component.ts
+// the table is used by the component to fill up the table component with data
+// the table will contain each row and column just ready to be used and output to the screen
+// the table is saved to src/app/dex/tableItems.ts
+
 import * as fs from 'fs';
 import Pokemon from '../src/app/shared/shared.module';
 
@@ -35,5 +44,9 @@ function saveToFile() {
   const path = './src/app/dex/tableItems.ts';
   const json = JSON.stringify(tableItems);
   const filetext = 'export const ROWS = ' + json + ';';
-  fs.writeFile(path, filetext, 'utf8', () => console.log('created json: ' + path));
+  fs.writeFile(path, filetext, 'utf8', (e) => {
+    console.log('creating json: ' + path);
+    if (e) { console.log('' + e);
+    } else { console.log('sucess'); }
+  });
 }
