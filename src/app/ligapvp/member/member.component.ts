456531bb2f6576d7ec5e39a0a27d7b8057c74c9e
Vandré Israel Brunazo <vibrunazo@gmail.com>
2019-08-29 18:52:22 -0300

    liga pvp component and members UI

--- src/app/ligapvp/member/member.component.ts
+++ src/app/ligapvp/member/member.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Player } from 'src/app/shared/ligapvp.module';
+import { Player, Liga } from 'src/app/shared/ligapvp.module';
+import { ActivatedRoute, Router, ParamMap } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-member',
@@ -7,12 +9,24 @@ import { Player } from 'src/app/shared/ligapvp.module';
   styleUrls: ['./member.component.scss']
 })
 export class MemberComponent implements OnInit {
-
   player: Player;
+  name = '';
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router) {
 
-  ngOnInit() {
   }
 
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      if (params.id) {
+        this.setMember(params.id);
+      }
+    });
+
+  }
+
+  setMember(member) {
+    this.name = member;
+    this.player = Liga.getPlayerByName(member);
+  }
 }
