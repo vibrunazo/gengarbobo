4e719ec76f4f9f3104949b0cd1d45fa3ec91af18
Vandré Israel Brunazo <vibrunazo@gmail.com>
2019-09-03 20:52:34 -0300

    lambda id on f1

--- src/app/shared/lambda.service.ts
+++ src/app/shared/lambda.service.ts
@@ -31,7 +31,7 @@ export class LambdaService {
     const httpOptions = {
       headers: new HttpHeaders({
         'Content-Type':  'application/json',
-        'Access-Control-Allow-Origin': '*'
+        Authorization: this.identity.get().currentUser().token
       })
     };
     // return this.http.get(this.url);
--- src_functions/testfunc.js
+++ src_functions/testfunc.js
@@ -1,6 +1,11 @@
 exports.handler = function(event, context, callback) {
   // your server-side functionality
   const { name } = JSON.parse(event.body);
+  const {identity, user} = context.clientContext;
+  console.log('id, user:');
+  console.log(identity);
+  console.log(user);
+
 
   callback(null, {
     "statusCode": 200,
