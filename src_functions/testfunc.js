c302045e5a2564591b259d96ed9cbf4d5c8b26eb
Vandré Israel Brunazo <vibrunazo@gmail.com>
2019-09-04 11:15:38 -0300

    env var

--- src_functions/testfunc.js
+++ src_functions/testfunc.js
@@ -1,22 +1,46 @@
-exports.handler = function(event, context, callback) {
-  // your server-side functionality
-  const { name } = JSON.parse(event.body);
-  const {identity, user} = context.clientContext;
-  console.log('id:');
-  console.log(identity);
-  console.log('user:');
-  console.log(user);
-  console.log('event:');
-  console.log(event);
-  // console.log('context:');
-  // console.log(context);
+const { MONGO_USER, MONGO_PSW, MONGO_URL } = process.env;
 
-
-  callback(null, {
-    "statusCode": 200,
-    "headers": {
-      "Access-Control-Allow-Origin": "*",
-    },
-    "body": JSON.stringify({msg: 'Hello ' + name}),
+exports.handler = async (event, context) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  const MongoClient = require('mongodb').MongoClient;
+  const uri = `mongodb+srv://${MONGO_USER}:${MONGO_PSW}@${MONGO_URL}`;
+  const client = new MongoClient(uri, { useNewUrlParser: true });
+  let collection;
+  await client.connect(err => {
+    collection = client.db("ligapvp").collection("members");
+    // perform actions on the collection object
+    client.close();
   });
-}
+  console.log('collection');
+  console.log(collection);
+
+
+  return {
+    statusCode: 200,
+    body: "Hello, " + collection
+  };
+
+
+};
+
+// exports.handler = function(event, context, callback) {
+//   // your server-side functionality
+//   const { name } = JSON.parse(event.body);
+//   const {identity, user} = context.clientContext;
+//   console.log('id:');
+//   console.log(identity);
+//   console.log('user:');
+//   console.log(user);
+//   // console.log('event:');
+//   // console.log(event);
+//   // console.log('context:');
+//   // console.log(context);
+
+//   callback(null, {
+//     "statusCode": 200,
+//     "headers": {
+//       "Access-Control-Allow-Origin": "*",
+//     },
+//     "body": JSON.stringify({msg: 'Hello ' + name}),
+//   });
+// }
