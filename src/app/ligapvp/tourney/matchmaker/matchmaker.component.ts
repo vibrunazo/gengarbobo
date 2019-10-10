65e935c8e1f2a43bec0dd72822ebad3a37779512
Vandré Israel Brunazo <vibrunazo@gmail.com>
2019-10-10 06:10:44 -0300

    random

--- src/app/ligapvp/tourney/matchmaker/matchmaker.component.ts
+++ src/app/ligapvp/tourney/matchmaker/matchmaker.component.ts
@@ -94,6 +94,32 @@ export class MatchmakerComponent implements OnInit {
     this.saveLs();
   }
 
+  onClickRandom() {
+    console.log('random');
+    const enemies = this.getEnemies();
+    const allowed = enemies.filter(e => this.canAddMatch(e, this.selectedPlayer));
+    console.log(allowed);
+    const length = allowed.length;
+    if (length === 0) { return; }
+    const rngPick = getRandomInt(0, length - 1);
+    const rngPlayer = allowed[rngPick];
+    this.onMatch(rngPlayer);
+
+    /**
+     * Returns a random integer between min (inclusive) and max (inclusive).
+     * The value is no lower than min (or the next integer greater than min
+     * if min isn't an integer) and no greater than max (or the next integer
+     * lower than max if max isn't an integer).
+     * Using Math.round() will give you a non-uniform distribution!
+     */
+    function getRandomInt(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+  }
+
   canAddMatch(p1: Player, p2: Player): boolean {
     const p1n = p1.getName();
     const p2n = p2.getName();
